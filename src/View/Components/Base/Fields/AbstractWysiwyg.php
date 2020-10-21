329cd9d91d465d53e7ebf882410145e3552ce7b5
robertkleinschuster <robert.kleinschuster@niceshops.com>
2020-10-21 20:18:29 +0200

    File and WYSIWYG Fields

--- src/View/Components/Base/Fields/AbstractWysiwyg.php
+++ src/View/Components/Base/Fields/AbstractWysiwyg.php
@@ -5,9 +5,34 @@ namespace Mvc\View\Components\Base\Fields;
 
 class AbstractWysiwyg extends AbstractTextarea
 {
+
+    public const TYPE_TOOLTIP = 'tooltip';
+    public const TYPE_TOOLBAR = 'toolbar';
+
+    private ?string $type;
+
+
     public function getTemplate()
     {
         return 'components/base/fields/wysiwyg';
     }
 
+    /**
+    * @return string
+    */
+    public function getType(): string
+    {
+        return $this->type ?? self::TYPE_TOOLBAR;
+    }
+
+    /**
+    * @param string $type
+    *
+    * @return $this
+    */
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+        return $this;
+    }
 }
